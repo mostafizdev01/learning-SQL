ee36f9ec639cf80f3129cd83d4c4571e5ef6c79b
Hera <mostafizhera@gmail.com>
2025-05-21 13:36:12 +0600

    creat view in sql

--- views.sql
+++ views.sql
@@ -1,1 +1,9 @@
+CREATE VIEW dept_avg_salary AS ---Wherever we need the salary value in this place, we will get it by simply calling the view Dept_Avig_Salary.
 SELECT department_name, avg(salary) FROM employeers GROUP BY department_name;
+
+SELECT * FROM dept_avg_salary;
+
+-- Why to use view ----
+-- 1. Simplifying complex queries
+-- 2. Improved security
+-- 3. Enhanced data abstraction.
